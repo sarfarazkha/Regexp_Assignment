ca7047c08ab16bab64c77aa93024f02ec0edc6d5
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-06-20 23:19:52 +0530

    user need to enter last name

---
+++ src/com/bridgelabz/regexp/RegExp2.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.regexp;
+
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegExp2 {
+    static Pattern pattern = Pattern.compile("[A-Z]{3,}$");
+
+    public static void main(String[] args) {
+        String testString = "khan";
+        Matcher matcher = pattern.matcher(testString);
+
+        if (matcher.matches()) {
+            System.out.println("matches");
+
+        } else {
+            System.out.println("not matching");
+        }
+    }
+}
