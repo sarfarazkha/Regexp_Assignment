f4fce6c41106ff8e4fb2fe64314f1cc98dd3fa7c
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-06-20 23:32:51 +0530

    user need to enter pasword atleast 1 uopercase

---
+++ src/com/bridgelabz/regexp/RegExp6.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.regexp;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegExp6 {
+    static Pattern pattern = Pattern.compile("([0-9]*[A-Z]){1,}[0-9a-zA-Z]{7,}$");
+
+    public static void main(String[] args) {
+        String testString = "Sarfaraz786";
+        Matcher matcher = pattern.matcher(testString);
+
+        if (matcher.matches()) {
+            System.out.println("matches");
+
+        } else {
+            System.out.println("not matching");
+        }
+    }
+}
+
