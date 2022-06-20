3d7439524e1c2615b12c7e2649d73c17b5225fd4
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-06-20 23:14:50 +0530

    user need to enter first name

---
+++ src/com/bridgelabz/regexp/RegExp1.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.regexp;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegExp1 {
+    static Pattern pattern = Pattern.compile("[A-Z]{3,}$");
+
+    public static void main(String[] args) {
+        String testString = "ABB";
+        Matcher matcher = pattern.matcher(testString);
+
+        if (matcher.matches()) {
+            System.out.println("matches");
+
+        } else {
+            System.out.println("not matching");
+        }
+    }
+}
