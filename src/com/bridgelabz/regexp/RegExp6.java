c3c402d2dfa65c90efd6af21ab0ea56b008c7db1
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-12-07 22:36:22 +0530

    Added: user need to enter pasword atleast 1 uppercase

--- src/com/bridgelabz/regexp/RegExp6.java
+++ src/com/bridgelabz/regexp/RegExp6.java
@@ -2,7 +2,7 @@ package com.bridgelabz.regexp;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class RegExp6 {
+public class RegExp{
     static Pattern pattern = Pattern.compile("([0-9]*[A-Z]){1,}[0-9a-zA-Z]{7,}$");
 
     public static void main(String[] args) {
