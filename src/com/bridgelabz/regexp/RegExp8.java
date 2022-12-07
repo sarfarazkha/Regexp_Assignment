e69030cc5c777afd5e68eb5513796805699f273d
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-12-07 22:39:50 +0530

    Added: Has exactly one special charecter

--- src/com/bridgelabz/regexp/RegExp8.java
+++ src/com/bridgelabz/regexp/RegExp8.java
@@ -3,7 +3,7 @@ package com.bridgelabz.regexp;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class RegExp8 {
+public class RegExp {
     static Pattern pattern = Pattern.compile("([0-9]*[A-Z]){3,}@[0-9a-zA-Z]$");
 
     public static void main(String[] args) {
@@ -17,7 +17,6 @@ public class RegExp8 {
             System.out.println("not matching");
 
         }
-
 
     }
 }
