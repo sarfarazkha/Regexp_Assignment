36b567f53b94515399056ae0cac7c9df8c32188b
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-12-07 22:30:32 +0530

    Added: user need to enter valid email

--- src/com/bridgelabz/regexp/RegExp3.java
+++ src/com/bridgelabz/regexp/RegExp3.java
@@ -3,7 +3,7 @@ package com.bridgelabz.regexp;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class RegExp3 {
+public class RegExp {
     static Pattern pattern = Pattern.compile("abc.[a-z]{3,}@bl.co.[a-z]{2}$");
 
     public static void main(String[] args) {
