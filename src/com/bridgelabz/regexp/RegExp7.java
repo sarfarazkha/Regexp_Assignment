35bbd5ea5f92e0eae89fda73b88e06824502a7fb
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-12-07 22:38:04 +0530

    Added: user need to enter pasword atleast 1 numeric

--- src/com/bridgelabz/regexp/RegExp7.java
+++ src/com/bridgelabz/regexp/RegExp7.java
@@ -3,7 +3,7 @@ package com.bridgelabz.regexp;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class RegExp7 {
+public class RegExp {
     static Pattern pattern = Pattern.compile("([0-9]*[A-Z]){1,}[0-9a-zA-Z]{7,}$");
 
     public static void main(String[] args) {
@@ -17,7 +17,5 @@ public class RegExp7 {
             System.out.println("not matching");
 
         }
-
-
-    }
-}
+     }
+ }
