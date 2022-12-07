840ab45073e6c34367b8caf24fc8a7c7b7b956a6
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-12-07 22:34:50 +0530

    Added: user need to enter pasword min 8 char

--- src/com/bridgelabz/regexp/RegExp5.java
+++ src/com/bridgelabz/regexp/RegExp5.java
@@ -3,7 +3,7 @@ package com.bridgelabz.regexp;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class RegExp5 {
+public class RegExp {
     static Pattern pattern = Pattern.compile("([0-9]*[a-zA-Z]){3,}[0-9a-zA-Z]*$");
 
     public static void main(String[] args) {
