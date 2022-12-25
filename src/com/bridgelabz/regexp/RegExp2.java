f6f8e424fb1a6955486af15ea83c7cf2e3d551aa
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-12-25 22:17:56 +0530

    Added: user need to enter last name

--- src/com/bridgelabz/regexp/RegExp2.java
+++ src/com/bridgelabz/regexp/RegExp2.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 public class RegExp2 {
     static Pattern pattern = Pattern.compile("[A-Z]{3,}$");
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         String testString = "khan";
         Matcher matcher = pattern.matcher(testString);
 
