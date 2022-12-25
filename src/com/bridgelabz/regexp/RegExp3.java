5b9db7896c474b6579dc11274c6335b942ac431d
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-12-25 22:19:36 +0530

    Added: user need to enter valid email

--- src/com/bridgelabz/regexp/RegExp3.java
+++ src/com/bridgelabz/regexp/RegExp3.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public class RegExp3 {
     static Pattern pattern = Pattern.compile("abc.[a-z]{3,}@bl.co.[a-z]{2}$");
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         String testString = "abc.zzz@bl.co.in";
         Matcher matcher = pattern.matcher(testString);
 
