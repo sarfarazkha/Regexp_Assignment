57bee473aa7975de6cb5780c32755df10e8db71f
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-12-25 22:16:57 +0530

    Added: User need to enter first name

--- src/com/bridgelabz/regexp/RegExp1.java
+++ src/com/bridgelabz/regexp/RegExp1.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public class RegExp1 {
     static Pattern pattern = Pattern.compile("[A-Z]{3,}$");
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         String testString = "ABB";
         Matcher matcher = pattern.matcher(testString);
 
